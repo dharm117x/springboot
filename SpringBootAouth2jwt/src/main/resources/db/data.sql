5d6c2732c980814d4422e5225c495cfbce71e393
Dharmendra Kuamr <34061027+dharm117x@users.noreply.github.com>
2024-03-19 17:14:20 +0530

    update oauth2 config

---
+++ SpringBootAouth2jwt/src/main/resources/db/data.sql
@@ -0,0 +1,1 @@
+insert into oauth_client_details (resource_ids) VALUES ('api')
---
+++ SpringBootAouth2jwt/src/main/resources/db/database.sql
@@ -0,0 +1,13 @@
+create table oauth_client_details (
+client_secret varchar(255), 
+resource_ids varchar(255), 
+scope varchar(255), 
+authorized_grant_types varchar(255),
+web_server_redirect_uri varchar(255), 
+authorities varchar(255),
+access_token_validity varchar(255),
+refresh_token_validity varchar(255),
+additional_information varchar(255), 
+autoapprove varchar(255), 
+client_id varchar(255)
+);
